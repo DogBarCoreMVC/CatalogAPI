c0fffdac88cf376ae3a8ad6f49f9f75d1bd3952a
GitDogBar <Dogbar>
2021-09-24 14:24:42 +0700

    Connection MongoDB

--- Repositories/MongoRepository.cs
+++ Repositories/MongoRepository.cs
@@ -1,7 +1,38 @@
+using System;
+using System.Collections.Generic;
+using CatelogVS.Entities;
+
 namespace CatelogVS.Repositories
 {
-    public class MongoRepository
+    public class MongoRepository : InterfaceRepository
     {
-        
+        public MongoRepository()
+        {
+            
+        }
+        public void CreateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
